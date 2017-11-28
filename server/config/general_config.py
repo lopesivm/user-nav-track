ce154487bb0e334bed06b1ce01b2856a16723907
Ivan Lopes <ivan.lopes@pearson.com>
2017-11-28 09:09:28 -0200

    - Replacing PostgreSQL for MySQL

--- server/config/general_config.py
+++ server/config/general_config.py
@@ -1,4 +1,4 @@
 import os
 
-DB_PATH = os.getenv('DATABASE_URL', 'sqlite:////tmp/database.db')
+DB_PATH = os.getenv('JAWSDB_URL', 'sqlite:////tmp/database.db')
 REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
