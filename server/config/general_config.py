ccf0495eb2432411be3f215f7876cca3e324f761
Ivan Lopes <ivan.lopes@pearson.com>
2017-11-28 06:05:01 -0200

    - Changed database to /tmp directory

--- server/config/general_config.py
+++ server/config/general_config.py
@@ -1,4 +1,4 @@
 import os
 
-DB_PATH = 'sqlite:////home/ilopes/workspace/personal/resultados_digitais/server/database.db'
+DB_PATH = 'sqlite:////tmp/database.db'
 REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
